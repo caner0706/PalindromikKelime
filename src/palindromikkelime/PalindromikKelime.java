fa858c79f035779b4ed08dfbcaedc9fb9c42bc1a
Caner Giden <canergiden7@icloud.com>
2023-06-20 21:10:10 +0300

    PalindromikKelime

---
+++ src/palindromikkelime/PalindromikKelime.java
@@ -0,0 +1,34 @@
+package palindromikkelime;
+
+public class PalindromikKelime {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(isPalindrome("abba"));
+        
+    }
+    
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    static boolean isPalindrome2(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
+    }
+    
+}
